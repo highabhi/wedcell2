d219e8d7738ac6f9dd95fd817092300e32ab7c73
highabhi <pr.abhi@icloud.com>
2022-10-29 20:11:53 +0530

    update 2
    updated

--- pages/Shop.js
+++ pages/Shop.js
@@ -1,127 +1,122 @@
-import React from 'react';
-import Styles from '../styles/Shop.module.css';
-import Link from 'next/link'
-
-
-
-
-
+import React from "react";
+import Styles from "../styles/Shop.module.css";
+import Link from "next/link";
 
 function Shop() {
+  const property = {
+    imageURL:
+      "https://image.wedmegood.com/resized-nw/1300X/wp-content/uploads/2019/05/56431137_3.jpg",
+    title: "item name",
+    itemDescription: "Bridal Wear Clothing Name",
+    button: "Shop",
+  };
 
-        const property = {
-            imageURL: 'https://image.wedmegood.com/resized-nw/1300X/wp-content/uploads/2019/05/56431137_3.jpg',
-            title : 'item name',
-            itemDescription: 'Bridal Wear Clothing Name',
-            button: 'Shop'
-        }
-
-        
-
-
-    return(
-        <>
-            
-            <div className="card">
-             <div className="card-body">
-                <div className="row g-6" >
-                    <Link href={"/ItemDetails"}>
-                    
-                    <div className="col-lg-4">
-                    <div className="card">
-                    <img className="card-img-top" src={property.imageURL} srcSet={property.imageURL} alt />
-                    <div className="card-body">
-                        <h5 className="card-title">Image Cap Top</h5>
-                        <p className="card-text">Etiam porta sem malesuada magna mollis euismod. Maecenas sed diam eget risus varius blandit sit amet non magna. Vivamus sagittis lacus augue.</p>
-                   
-                    </div>
-                  
+  return (
+    <div>
+      <div className="card">
+        <div className="card-body">
+          <div className="row g-6">
+            <Link href={"/ItemDetails"}>
+              <div className="col-lg-4">
+                <div className="card">
+                  <img
+                    className="card-img-top"
+                    src={property.imageURL}
+                    srcSet={property.imageURL}
+                    alt
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">Image Cap Top</h5>
+                    <p className="card-text">{property.title}</p>
+                  </div>
                 </div>
+              </div>
+            </Link>
+
+            <div className="col-lg-4">
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src={property.imageURL}
+                  srcSet={property.imageURL}
+                  alt
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Image Cap Top</h5>
+                  <p className="card-text">{property.title}</p>
+                </div>
+                {/*/.card-body */}
+              </div>
+              {/*/.card */}
             </div>
-                </Link>
-               
-                <div className="col-lg-4">
-                    <div className="card">
-                    <img className="card-img-top" src={property.imageURL} srcSet={property.imageURL} alt />
-                    <div className="card-body">
-                        <h5 className="card-title">Image Cap Top</h5>
-                        <p className="card-text">Etiam porta sem malesuada magna mollis euismod. Maecenas sed diam eget risus varius blandit sit amet non magna. Vivamus sagittis lacus augue.</p>
-                    </div>
-                    {/*/.card-body */}
-                    </div>
-                    {/*/.card */}
+            <div className="col-lg-4">
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src={property.imageURL}
+                  srcSet={property.imageURL}
+                  alt
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Image Cap Top</h5>
+                  <p className="card-text">{property.title}</p>
                 </div>
-                <div className="col-lg-4">
-                    <div className="card">
-                    <img className="card-img-top" src={property.imageURL} srcSet={property.imageURL} alt />
-                    <div className="card-body">
-                        <h5 className="card-title">Image Cap Top</h5>
-                        <p className="card-text">Etiam porta sem malesuada magna mollis euismod. Maecenas sed diam eget risus varius blandit sit amet non magna. Vivamus sagittis lacus augue.</p>
-                    </div>
-                    {/*/.card-body */}
-                    </div>
-                    {/*/.card */}
-                </div>
-
-                
-                {/* /column */}
-                </div>
-                {/* /.row */}
+              </div>
             </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="card-body">
+          <div className="row g-6">
+            <div className="col-lg-4">
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src={property.imageURL}
+                  srcSet={property.imageURL}
+                  alt
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Image Cap Top</h5>
+                  <p className="card-text">{property.title}</p>
+                </div>
+              </div>
             </div>
-
-            <div className="card">
-            <div className="card-body">
-                <div className="row g-6">
-                <div className="col-lg-4">
-                    <div className="card">
-                    <img className="card-img-top" src={property.imageURL} srcSet={property.imageURL} alt />
-                    <div className="card-body">
-                        <h5 className="card-title">Image Cap Top</h5>
-                        <p className="card-text">Etiam porta sem malesuada magna mollis euismod. Maecenas sed diam eget risus varius blandit sit amet non magna. Vivamus sagittis lacus augue.</p>
-                    </div>
-                    {/*/.card-body */}
-                    </div>
-                    {/*/.card */}
+            <div className="col-lg-4">
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src={property.imageURL}
+                  srcSet={property.imageURL}
+                  alt
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Image Cap Top</h5>
+                  <p className="card-text">{property.title}</p>
                 </div>
-                {/* /column */}
-                <div className="col-lg-4">
-                    <div className="card">
-                    <img className="card-img-top" src={property.imageURL} srcSet={property.imageURL} alt />
-                    <div className="card-body">
-                        <h5 className="card-title">Image Cap Top</h5>
-                        <p className="card-text">Etiam porta sem malesuada magna mollis euismod. Maecenas sed diam eget risus varius blandit sit amet non magna. Vivamus sagittis lacus augue.</p>
-                    </div>
-                    {/*/.card-body */}
-                    </div>
-                    {/*/.card */}
-                </div>
-                <div className="col-lg-4">
-                    <div className="card">
-                    <img className="card-img-top" src={property.imageURL} srcSet={property.imageURL} alt />
-                    <div className="card-body">
-                        <h5 className="card-title">Image Cap Top</h5>
-                        <p className="card-text">Etiam porta sem malesuada magna mollis euismod. Maecenas sed diam eget risus varius blandit sit amet non magna. Vivamus sagittis lacus augue.</p>
-                    </div>
-                    {/*/.card-body */}
-                    </div>
-                    {/*/.card */}
-                </div>
-
-                
-                {/* /column */}
-                </div>
-                {/* /.row */}
+              </div>
             </div>
+            <div className="col-lg-4">
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src={property.imageURL}
+                  srcSet={property.imageURL}
+                  alt
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Image Cap Top</h5>
+                  <p className="card-text">{property.title}</p>
+                </div>
+              </div>
             </div>
-
-                                
-
-
-
-
-    </>
-    )
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Shop;
--- pages/WeddingProfile.js
+++ pages/WeddingProfile.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import Styles from "../styles/weddingprofile.module.css";
 import Link from "next/link";
-import Image from "next/image";
+// import Image from "next/image";
 import VStyles from "../styles/Vendors.module.css";
-
 import Gallery from "../Components/Gallery";
 import Albums from "../Components/Albums";
 import Videos from "../Components/Videos";
+
+
 
 function WeddingProfile() {
   const property = {
@@ -17,82 +18,92 @@ function WeddingProfile() {
   };
 
   const [activeTab, setActiveTab] = useState("gallery");
- 
+
   return (
     <>
       <div className={Styles.container}>
         <div className="col-sm-4 w-75 rounded ">
           <div className={Styles.Card}>
-              
             <img
               className="card-img-top"
               src={property.imageURL}
               srcSet={property.imageURL}
               alt
             />
-            
           </div>
         </div>
       </div>
+     
+     <div className="container">
+     bootstrap container
+    </div>
 
-    <div style={{
-        marginTop: "250px"
-    }}>
 
-      <div className="container py-5">
-        <div className="row">
-          <div className="col-md-12">
-            <div className="gallery-container pb-5 bg-white box-shadow mb-4">
-              <div className="tabs-container d-flex align-items-center">
-                <div
-                  className={VStyles.tab}
-                  style={{
-                    backgroundColor:
-                      activeTab === "gallery" ? "#fff" : "#f8f8f8",
-                    color:
-                      activeTab === "gallery" ? "hsla(0, 70%, 24%, 1)" : "#000",
-                  }}
-                  onClick={() => setActiveTab("gallery")}
-                >
-                  <h5 className="fw-bold">Gallery </h5>
-                </div>
-                <div
-                  className={VStyles.tab}
-                  style={{
-                    backgroundColor:
-                      activeTab === "albums" ? "#fff" : "#f8f8f8",
-                    color:
-                      activeTab === "albums" ? "hsla(0, 70%, 24%, 1)" : "#000",
-                  }}
-                  onClick={() => setActiveTab("albums")}
-                >
-                  <h5 className="fw-bold">Albums </h5>
-                </div>
-                <div
-                  className={VStyles.tab}
-                  style={{
-                    backgroundColor:
-                      activeTab === "videos" ? "#fff" : "#f8f8f8",
-                    color:
-                      activeTab === "videos" ? "hsla(0, 70%, 24%, 1)" : "#000",
-                  }}
-                  onClick={() => setActiveTab("videos")}
-                >
-                  <h5 className="fw-bold">Videos</h5>
-                </div>
-              </div>
-              <div className="active-comp px-4 mt-4">
-                {activeTab == "gallery" && <Gallery />}
-                {activeTab == "albums" && <Albums />}
 
-                {activeTab == "videos" && <Videos />}
+      <div
+        style={{
+          marginTop: "250px",
+        }}
+      >
+        <div className="container py-5">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="gallery-container pb-5 bg-white box-shadow mb-4">
+                <div className="tabs-container d-flex align-items-center">
+                  <div
+                    className={VStyles.tab}
+                    style={{
+                      backgroundColor:
+                        activeTab === "gallery" ? "#fff" : "#f8f8f8",
+                      color:
+                        activeTab === "gallery"
+                          ? "hsla(0, 70%, 24%, 1)"
+                          : "#000",
+                    }}
+                    onClick={() => setActiveTab("gallery")}
+                  >
+                    <h5 className="fw-bold">Gallery </h5>
+                  </div>
+                  <div
+                    className={VStyles.tab}
+                    style={{
+                      backgroundColor:
+                        activeTab === "albums" ? "#fff" : "#f8f8f8",
+                      color:
+                        activeTab === "albums"
+                          ? "hsla(0, 70%, 24%, 1)"
+                          : "#000",
+                    }}
+                    onClick={() => setActiveTab("albums")}
+                  >
+                    <h5 className="fw-bold">Albums </h5>
+                  </div>
+                  <div
+                    className={VStyles.tab}
+                    style={{
+                      backgroundColor:
+                        activeTab === "videos" ? "#fff" : "#f8f8f8",
+                      color:
+                        activeTab === "videos"
+                          ? "hsla(0, 70%, 24%, 1)"
+                          : "#000",
+                    }}
+                    onClick={() => setActiveTab("videos")}
+                  >
+                    <h5 className="fw-bold">Videos</h5>
+                  </div>
+                </div>
+                <div className="active-comp px-4 mt-4">
+                  {activeTab == "gallery" && <Gallery />}
+                  {activeTab == "albums" && <Albums />}
+
+                  {activeTab == "videos" && <Videos />}
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-
-    </div>
 
       <div className={Styles.profilecontainer}>
         <div className="card">
